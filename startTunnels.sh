12761af0c41a7282cc7b27c79f33ca5c2e1d8910
AlejandroHerr <hola@alejandroherr.io>
2021-11-25 16:23:20 +0100

    fix(autossh): add ExitOnForwardFailure option

--- startTunnels.sh
+++ startTunnels.sh
@@ -6,4 +6,4 @@ pkill -f parallel
 export AUTOSSH_LOGFILE="/logs/autossh.log"
 export AUTOSSH_LOGLEVEL="7"
 
-parallel -j 100 --verbose -u autossh -M 0 -o \"StrictHostKeyChecking no\" -o \"ServerAliveInterval 30\" -o \"ServerAliveCountMax 3\" -NR {} $REMOTE_USER@$HOST -E /logs/ssh.log -v -i /ssh/id_rsa </tunnels &
+parallel -j 100 --verbose -u autossh -M 0 -o \"ExitOnForwardFailure yes\" -o \"StrictHostKeyChecking no\" -o \"ServerAliveInterval 30\" -o \"ServerAliveCountMax 3\" -NR {} $REMOTE_USER@$HOST -E /logs/ssh.log -v -i /ssh/id_rsa </tunnels &
